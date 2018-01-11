ab89108358f1f54bd6ae45c604958b41cf452f84
ealgase <mostdigitsofpi@gmail.com>
2018-01-11 19:20:10 +0000

    Add other input functions for game
    Now, we can take an array and automatically generate a prompt as well as
    make sure all of the inputs are valid for the user to type (e.g. not
    int)

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ import random
 import time
 
 #libs imported, now we can make the function for taking input, hope this works
-def input(input_array,input_q):
+def input_gnui(input_array,input_q):
     input_t=False
     while input_t not in input_array:
-        input_t=input(input_q)
+        input_t=input(input_q).upper()
     return input_t
     #that was just a simple function for getting input with an array, the main UI one will come next
+def input_gnui_wm(input_array,input_q,input_m):
+    print(input_m)
+    return input_gnui(input_array,input_q)
+    #this function adds a message which will usually be used but not always
+def input_main(input_array,input_m):
+    #this automatically generates an input_q
+    input_array_r=[]
+    input_q_r=""
+    for item in input_array:
+        input_array_r.append(str(item).upper())
+        input_q_r+=str(item).upper()+", "
+    input_q_r=input_q_r[:-2]+": "
+    return input_gnui_wm(input_array_r,input_q_r,input_m)
